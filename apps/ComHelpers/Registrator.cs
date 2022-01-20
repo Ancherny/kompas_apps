29877d88c886b2c74217692c7e7882fd5041ad3e
ancher <ancher@mail.ru>
2022-01-20 21:03:23 +0300

    my first kompas app

---
+++ apps/ComHelpers/Registrator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace ComHelpers
+{
+    public class Registrator
+    {
+        private const string regPath = @"SOFTWARE\Classes\CLSID\";
+        private const string kompasLibName = "Kompas_Library";
+        private const string interopKeyName = "InprocServer32";
+        private const string interopKeyValue = @"\mscoree.dll";
+
+        private readonly string _typeKeyName;
+        private readonly string _interopDllPath;
+
+        public Registrator(Type t)
+        {
+            _typeKeyName = $"{regPath}{{{t.GUID}}}";
+            _interopDllPath = Environment.GetFolderPath(Environment.SpecialFolder.System) + interopKeyValue;
+        }
+
+        public void RegisterKompasLib()
+        {
+            try
+            {
+                using RegistryKey typeKey = Registry.LocalMachine.OpenSubKey(_typeKeyName, true);
+                if (typeKey != null)
+                {
+                    using RegistryKey libKey = typeKey.CreateSubKey(kompasLibName);
+                    using RegistryKey interopKey = typeKey.OpenSubKey(interopKeyName, true);
+                    if (interopKey != null)
+                    {
+                        typeKey.SetValue(null, _interopDllPath);
+                    }
+                    else
+                    {
+                        throw new Exception("Failed to get registry key " + interopKeyName);
+                    }
+                }
+                else
+                {
+                    throw new Exception("Failed to get registry key " + _typeKeyName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format($"Error registering Kompas class for COM-Interop:\n{ex}"));
+            }
+        }
+		
+        public void UnregisterKompasLib()
+        {
+            try
+            {
+                using RegistryKey typeKey = Registry.LocalMachine.OpenSubKey(_typeKeyName, true);
+                if (typeKey != null)
+                {
+                    RegistryKey libKey = typeKey.OpenSubKey(kompasLibName);
+                    if (libKey != null)
+                    {
+                        libKey.Close();
+                        typeKey.DeleteSubKey(kompasLibName);
+                    }
+                }
+                else
+                {
+                    throw new Exception("Failed to get registry key " + _typeKeyName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format($"Error un-registering Kompas class for COM-Interop:\n{ex}"));
+            }
+        }
+    }
+}
---
+++ apps/Test/Hello.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Kompas6API5;
+using ComHelpers;
+
+namespace Test
+{
+    // ReSharper disable once UnusedType.Global
+    // ReSharper disable once ClassNeverInstantiated.Global
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class Hello
+    {
+        // ReSharper disable once UnusedMember.Global
+        [return: MarshalAs(UnmanagedType.BStr)]
+        public string GetLibraryName()
+        {
+            return "Hello - the first kompas app";
+        }
+
+        /// <summary>
+        /// Main app entry
+        /// </summary>
+        // ReSharper disable once UnusedMember.Global
+        public void ExternalRunCommand(
+            // ReSharper disable once UnusedParameter.Global
+            [In] short command,
+            // ReSharper disable once UnusedParameter.Global
+            [In] short mode,
+            [In, MarshalAs(UnmanagedType.IDispatch)] object kompasObj)
+        {
+            KompasObject kompas = (KompasObject) kompasObj;
+            kompas.ksMessage("Hello Kompas!");
+        }
+
+        #region COM Registration
+        [ComRegisterFunction]
+        public static void RegisterKompasLib(Type t)
+        {
+            new Registrator(t).RegisterKompasLib();
+        }
+		
+        [ComUnregisterFunction]
+        public static void UnregisterKompasLib(Type t)
+        {
+            new Registrator(t).UnregisterKompasLib();
+        }
+        #endregion
+    }
+}
