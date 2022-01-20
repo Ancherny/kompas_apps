1641d57412f966ac41f0d9c58b874df589eefad3
ancher <ancher@mail.ru>
2022-01-21 00:07:15 +0300

    move inits to BaseCommand

---
+++ apps/Test/BaseCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using ComHelpers;
+using JetBrains.Annotations;
+
+namespace Test
+{
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public abstract class BaseCommand
+    {
+        private readonly string _libName;
+        
+        protected BaseCommand([NotNull] string libName)
+        {
+            _libName = libName;
+        }
+        
+        protected abstract void Action(short command, short mode, object kompasObj);
+        
+        // ReSharper disable once UnusedMember.Global
+        [return: MarshalAs(UnmanagedType.BStr)]
+        public string GetLibraryName()
+        {
+            return _libName;
+        }
+
+        /// <summary>
+        /// Main app entry
+        /// </summary>
+        // ReSharper disable once UnusedMember.Global
+        public void ExternalRunCommand(
+            // ReSharper disable once UnusedParameter.Global
+            [In] short command,
+            // ReSharper disable once UnusedParameter.Global
+            [In] short mode,
+            [In, MarshalAs(UnmanagedType.IDispatch)] object kompasObj)
+        {
+            Action(command, mode, kompasObj);
+        }
+
+        #region COM Registration
+        [ComRegisterFunction]
+        public static void RegisterKompasLib(Type t)
+        {
+            new Registrator(t).RegisterKompasLib();
+        }
+		
+        [ComUnregisterFunction]
+        public static void UnregisterKompasLib(Type t)
+        {
+            new Registrator(t).UnregisterKompasLib();
+        }
+        #endregion
+    }
+}
--- apps/Test/Hello.cs
+++ apps/Test/Hello.cs
@@ -1,49 +1,18 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using Kompas6API5;
-using ComHelpers;
+﻿using Kompas6API5;
 
 namespace Test
 {
     // ReSharper disable once UnusedType.Global
-    // ReSharper disable once ClassNeverInstantiated.Global
-    [ClassInterface(ClassInterfaceType.AutoDual)]
-    public class Hello
+    public class Hello : BaseCommand
     {
-        // ReSharper disable once UnusedMember.Global
-        [return: MarshalAs(UnmanagedType.BStr)]
-        public string GetLibraryName()
+        public Hello() : base("Hello - the first kompas app")
         {
-            return "Hello - the first kompas app";
         }
 
-        /// <summary>
-        /// Main app entry
-        /// </summary>
-        // ReSharper disable once UnusedMember.Global
-        public void ExternalRunCommand(
-            // ReSharper disable once UnusedParameter.Global
-            [In] short command,
-            // ReSharper disable once UnusedParameter.Global
-            [In] short mode,
-            [In, MarshalAs(UnmanagedType.IDispatch)] object kompasObj)
+        protected override void Action(short command, short mode, object kompasObj)
         {
             KompasObject kompas = (KompasObject) kompasObj;
-            kompas.ksMessage("Hello Kompas!");
+            kompas.ksMessage("Hello zzz Kompas!");
         }
-
-        #region COM Registration
-        [ComRegisterFunction]
-        public static void RegisterKompasLib(Type t)
-        {
-            new Registrator(t).RegisterKompasLib();
-        }
-		
-        [ComUnregisterFunction]
-        public static void UnregisterKompasLib(Type t)
-        {
-            new Registrator(t).UnregisterKompasLib();
-        }
-        #endregion
     }
 }
