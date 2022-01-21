1303cd3f2bba80109a9919619ddd085dd89ed466
ancher <ancher@mail.ru>
2022-01-21 22:59:41 +0300

    dummy part creation added
    (cherry picked from commit dab83c811e58b8f0bf6149f485b83caee2b45d2e)

---
+++ apps/CreatePart/CreatePart.cs
@@ -0,0 +1,34 @@
+﻿using Kompas6API5;
+using Base;
+
+namespace RunCommands
+{
+    // ReSharper disable once UnusedType.Global
+    public class CreatePart : BaseCommand
+    {
+        public CreatePart() : base("API7 3D test")
+        {
+        }
+    
+        protected override void Action(short command, short mode, object kompasObj)
+        {
+            KompasObject kompas = (KompasObject)kompasObj;
+            bool isSuccess;
+            do
+            {
+                Document3D d3d;
+                isSuccess = DocHelpers.CreateDoc3D(out d3d, kompas, DocHelpers.Doc3DType.Part);
+
+            } while (false);
+
+            if (isSuccess)
+            {
+                kompas.ksMessage("");
+            }
+            else
+            {
+                kompas.ksMessage("Hello Kompas!");
+            }
+        }
+    }
+}
