57ecea137bd3ee6237821760ec7660812b6835d3
ancher <ancher@mail.ru>
2022-01-21 23:00:08 +0300

    dummy part creation added
    (cherry picked from commit b8933802a280a46d5538cadf433148182f145fc6)

---
+++ apps/Base/DocHelpers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using JetBrains.Annotations;
+using Kompas6API5;
+using KAPITypes;
+using Kompas6Constants;
+
+namespace Base;
+
+public static class DocHelpers
+{
+    public enum Doc3DType
+    {
+        Part,
+        Assembly
+    }
+    
+    public static void CreateDoc2D(object kompasObj)
+    {
+        var kompas = (KompasObject)kompasObj;
+            
+        var dp = (DocumentParam)kompas.GetParamStruct((short)StructType2DEnum.ko_DocumentParam);;
+        dp.Init();
+        dp.type = (short)DocType.lt_DocPart3D;
+
+        var d2d = (Document2D)kompas.Document2D();
+        d2d.ksCreateDocument(dp);
+        kompas.ksMessage("Doc2D created!");
+    }
+
+    public static bool CreateDoc3D(out Document3D d3d, [NotNull] KompasObject kompas, Doc3DType doc3DType)
+    {
+        d3d = null;
+        bool isSuccess;
+        d3d = (Document3D)kompas.Document3D();
+        switch (doc3DType)
+        {
+            case Doc3DType.Part:
+                isSuccess = d3d.Create(false, true);
+                break;
+            case Doc3DType.Assembly:
+                isSuccess = d3d.Create(false, false);
+                break;
+            default:
+                isSuccess = false;
+                break;
+        }
+        return isSuccess;
+    }
+}
--- apps/CreatePart/CreatePart.cs
+++ apps/CreatePart/CreatePart.cs
@@ -3,19 +3,24 @@ using Base;
 
 namespace RunCommands
 {
+    /// <summary>
+    /// Create new part with english namings
+    /// </summary>
+    ///
     // ReSharper disable once UnusedType.Global
     public class CreatePart : BaseCommand
     {
         public CreatePart() : base("API7 3D test")
         {
         }
-    
+
         protected override void Action(short command, short mode, object kompasObj)
         {
             KompasObject kompas = (KompasObject)kompasObj;
             bool isSuccess;
             do
             {
+                // ReSharper disable once NotAccessedVariable
                 Document3D d3d;
                 isSuccess = DocHelpers.CreateDoc3D(out d3d, kompas, DocHelpers.Doc3DType.Part);
 
