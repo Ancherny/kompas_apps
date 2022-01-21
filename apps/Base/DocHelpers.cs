f455363b4ac3cad2b41922ae6fd2ecc424567b37
ancher <ancher@mail.ru>
2022-01-22 00:11:57 +0300

    Move creation to DocHelpers
    (cherry picked from commit ea216613b0012a933d1db884ceab6df2b79fbd26)

--- apps/Base/DocHelpers.cs
+++ apps/Base/DocHelpers.cs
@@ -14,19 +14,20 @@ public static class DocHelpers
         Assembly
     }
 
+    // ReSharper disable once UnusedMember.Global
     public static void CreateDoc2D(object kompasObj)
     {
         var kompas = (KompasObject)kompasObj;
 
-        var dp = (DocumentParam)kompas.GetParamStruct((short)StructType2DEnum.ko_DocumentParam);;
+        var dp = (DocumentParam)kompas.GetParamStruct((short)StructType2DEnum.ko_DocumentParam);
         dp.Init();
         dp.type = (short)DocType.lt_DocPart3D;
 
         var d2d = (Document2D)kompas.Document2D();
         d2d.ksCreateDocument(dp);
-        kompas.ksMessage("Doc2D created!");
     }
 
+    // ReSharper disable once MemberCanBePrivate.Global
     public static bool CreateDoc3D(out Document3D d3d, [NotNull] KompasObject kompas, Doc3DType doc3DType)
     {
         d3d = null;
@@ -35,7 +36,7 @@ public static class DocHelpers
         switch (doc3DType)
         {
             case Doc3DType.Part:
-                isSuccess = d3d.Create(false, true);
+                isSuccess = d3d.Create();
                 break;
             case Doc3DType.Assembly:
                 isSuccess = d3d.Create(false, false);
@@ -47,11 +48,35 @@ public static class DocHelpers
         return isSuccess;
     }
 
-    public static void SetPartFirstEntityName([NotNull] ksPart part, [NotNull] string name, Obj3dType entityType)
+    private static void SetPartFirstEntityName([NotNull] ksPart part, [NotNull] string name, Obj3dType entityType)
     {
         var collection = (ksEntityCollection)part.EntityCollection((short)entityType);
         var entity = (ksEntity)collection.GetByIndex(0);
         entity.name = name;
         entity.Update();
+    }
+
+    public static bool CreateNew(KompasObject kompas, Doc3DType docType)
+    {
+        bool isSuccess;
+        do
+        {
+            Document3D d3d;
+            isSuccess = CreateDoc3D(out d3d, kompas, docType);
+            var topPart = (ksPart)d3d.GetPart((int)Part_Type.pTop_Part);
+            topPart.name = docType.ToString();
+            topPart.Update();
+
+            SetPartFirstEntityName(topPart, "Origin", Obj3dType.o3d_pointCS);
+            SetPartFirstEntityName(topPart, "Plane_XY", Obj3dType.o3d_planeXOY);
+            SetPartFirstEntityName(topPart, "Plane_XZ", Obj3dType.o3d_planeXOZ);
+            SetPartFirstEntityName(topPart, "Plane_YZ", Obj3dType.o3d_planeYOZ);
+            SetPartFirstEntityName(topPart, "Axis_X", Obj3dType.o3d_axisOX);
+            SetPartFirstEntityName(topPart, "Axis_X", Obj3dType.o3d_axisOY);
+            SetPartFirstEntityName(topPart, "Axis_X", Obj3dType.o3d_axisOZ);
+
+        } while (false);
+
+        return isSuccess;
     }
 }
--- apps/CreatePart/CreatePart.cs
+++ apps/CreatePart/CreatePart.cs
@@ -1,5 +1,4 @@
 ﻿using Kompas6API5;
-using Kompas6Constants3D;
 using Base;
 namespace RunCommands
 {
@@ -17,26 +16,7 @@ namespace RunCommands
         protected override void Action(short command, short mode, object kompasObj)
         {
             KompasObject kompas = (KompasObject)kompasObj;
-            bool isSuccess;
-            do
-            {
-                Document3D d3d;
-                isSuccess = DocHelpers.CreateDoc3D(out d3d, kompas, DocHelpers.Doc3DType.Part);
-                var topPart = (ksPart)d3d.GetPart((int)Part_Type.pTop_Part);
-                topPart.name = "Part";
-                topPart.Update();
-
-                DocHelpers.SetPartFirstEntityName(topPart, "Origin", Obj3dType.o3d_pointCS);
-                DocHelpers.SetPartFirstEntityName(topPart, "Plane_XY", Obj3dType.o3d_planeXOY);
-                DocHelpers.SetPartFirstEntityName(topPart, "Plane_XZ", Obj3dType.o3d_planeXOZ);
-                DocHelpers.SetPartFirstEntityName(topPart, "Plane_YZ", Obj3dType.o3d_planeYOZ);
-                DocHelpers.SetPartFirstEntityName(topPart, "Axis_X", Obj3dType.o3d_axisOX);
-                DocHelpers.SetPartFirstEntityName(topPart, "Axis_X", Obj3dType.o3d_axisOY);
-                DocHelpers.SetPartFirstEntityName(topPart, "Axis_X", Obj3dType.o3d_axisOZ);
-
-            } while (false);
-
-            if (!isSuccess)
+            if (!DocHelpers.CreateNew(kompas, DocHelpers.Doc3DType.Part))
             {
                 kompas.ksMessage("Failed to create new part.");
             }
